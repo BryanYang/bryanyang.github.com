b4893d3676e6819438d3785235c006b6830df579
bryanyang <249763186@qq.com>
2016-02-22 17:31:51 +0800

    update

--- build/build.js
+++
@@ -1,2 +0,0 @@
-var lines=[];
-React.render(React.createElement(Article, {author: "Bryan Yang", date:"2015-12-11 18:27:06", lines: lines}),document.getElementById("article"));
--- build/my_select2.js
+++
@@ -1,2 +0,0 @@
-var lines=['# React select插件','## 更新了。支持搜索。以及分类和批量选择','<iframe src="test.html" style="width: 800px; height: 600px; border: none;"></iframe>','-------','','','任何建议或者意见请联系作者 :) [yangggao@hotmail.com](Mailto:yangggao@hotmail.com)','   ',''];
-React.render(React.createElement(Article, {author: "Bryan Yang", date:"2015-12-16 17:30:46", lines: lines}),document.getElementById("article"));
